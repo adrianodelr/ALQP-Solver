8f33db246af42b0d0723684e738bcd80b02bb543
adrianodelr <noche96@gmx.de>
2024-09-03 01:55:02 +0200

    degenerate AL hessian detection added as status variable

--- src/ALQPSolver.h
+++ src/ALQPSolver.h
@@ -11,7 +11,7 @@ namespace ALQPS {
 template<int nx, int m, int p>
 class QPsol{
     public:
-        QPsol(Matrix<nx,1> x_, Matrix<m,1> lambda_, Matrix<p,1> mu_, float obj_val_, bool solved, bool pinf, bool dinf, bool verbose) : x(x_), lambda(lambda_), mu(mu_), obj_val(obj_val_), _solved(solved), _pinf(pinf), _dinf(dinf) {
+        QPsol(Matrix<nx,1> x_, Matrix<m,1> lambda_, Matrix<p,1> mu_, float obj_val_, bool solved, bool pinf, bool dinf, bool degh, bool verbose) : x(x_), lambda(lambda_), mu(mu_), obj_val(obj_val_), _solved(solved), _pinf(pinf), _dinf(dinf), _degh(degh) {
             if (verbose) print_report();
         }; 
 
@@ -23,7 +23,8 @@ class QPsol{
         const float obj_val;         
         const bool _solved;
         const bool _pinf;
-        const bool _dinf;  
+        const bool _dinf;
+        const bool _degh;   
 
         void print_report(){
             
@@ -31,6 +32,7 @@ class QPsol{
             if (_solved) Serial.println("solved");
             else if (_pinf) Serial.println("primal infeasible");
             else if (_dinf) Serial.println("dual infeasible");  
+            else if (_degh) Serial.println("singular Hessian");  
 
             Serial.print("optimal objective: ");
             Serial.println(obj_val);
@@ -43,7 +45,6 @@ class QPsol{
 
             Serial.print("dual value (inequalities): ");
             Serial.println(mu);
-
         }          
 };
 
@@ -61,6 +62,7 @@ class QP{
         }; 
         // Default constructor for empty qp
         QP(){
+            // TODO: handle unconstrained problem properly
             Matrix<nx,nx> Q;
             Matrix<nx,1>  q;
             Matrix<m ,nx> A;
@@ -76,7 +78,7 @@ class QP{
             update(Q, q, A, b, G, h);
 
             // fixed solver settings 
-            max_iter_newton = 10;
+            max_iter_newton = 25;
             max_iter_outer  = 10;
 
             precision_newton = 1e-8;
@@ -106,7 +108,16 @@ class QP{
             double normrp;
 
             for (int i = 0; i < max_iter_outer; i++){
-                x = newton_solve(x, lambda, mu, rho);
+                x = newton_solve(x, lambda, mu, rho, verb);
+                if (isnan(x(0))){
+                    QPsol<nx,m,p> sol(x,lambda,mu,0.0/0.0,false,false,false,true,verb);
+                    return sol;
+                }
+                // if (m+p == 0){
+                //     QPsol<nx,m,p> sol(x,lambda,mu,objective(x),true,false,false,false,verb);
+                //     return sol;
+                // }
+                
                 // update dual variables lambda, mu
                 dual_update(x, lambda, mu, rho); 
                 rho = Phi*rho;
@@ -115,17 +126,17 @@ class QP{
                 normrp = sqrt(innerprod(0));
                 // verify if subset of KKT conditions (primal+dual feasibility) are satisfied 
                 if (normrp < precision_primal && dual_feasibility(mu)){
-                    QPsol<nx,m,p> sol(x,lambda,mu,objective(x),true,false,false,verb);                    
+                    QPsol<nx,m,p> sol(x,lambda,mu,objective(x),true,false,false,false,verb);                    
                     return sol;
                 }
             }
             x.Fill(0.0/0.0);
             if (normrp > precision_primal){
-                QPsol<nx,m,p> sol(x,lambda,mu,0.0/0.0,false,true,false,verb);
+                QPsol<nx,m,p> sol(x,lambda,mu,0.0/0.0,false,true,false,false,verb);
                 return sol;                     
             }
             else if (!dual_feasibility(mu)){
-                QPsol<nx,m,p> sol(x,lambda,mu,0.0/0.0,false,false,true,verb);
+                QPsol<nx,m,p> sol(x,lambda,mu,0.0/0.0,false,false,true,false,verb);
                 return sol;
             }
         };
@@ -150,11 +161,11 @@ class QP{
 
         // returns left hand side of the equality constraints   
         Matrix<m,1> c_eq(Matrix<nx,1> x){
-            return _A*x - _b;
+            return _A*x - _b; 
         };
         // returns left hand side of the inequality constraints   
         Matrix<p,1> c_in(Matrix<nx,1> x){
-            return _G*x - _h;
+            return _G*x - _h; 
         };
         // computes the objective value 
         float objective(Matrix<nx,1> x){
@@ -187,7 +198,7 @@ class QP{
             for (int i = 0; i < p; i++){
                 if(mu(i) < 0.0) return false;
             }
-            return true;
+            return true;  
         }
 
         void dual_update(Matrix<nx,1> x, Matrix<m,1> &lambda, Matrix<p> &mu, float rho){
@@ -196,11 +207,9 @@ class QP{
             for (int i = 0; i < p; i++){
                 mu(i) = max(0, mu(i)+rho*c(i));
             }
-            if (m != 0){
-                for (int i = 0; i < p; i++){
-                    lambda(i) = lambda(i)+rho*h(i);
-                }
-            }            
+            for (int i = 0; i < m; i++){
+                lambda(i) = lambda(i)+rho*h(i);
+            }  
         };
         
         Matrix<p,p> active_set(Matrix<nx,1> x, Matrix<p,1> mu, float rho){
@@ -215,7 +224,7 @@ class QP{
                     Ip(i,i) = rho;
                 }
             }
-            return Ip;            
+            return Ip;           
         };
 
         Matrix<nx, 1> algradient(Matrix<nx,1> x, Matrix<m,1> lambda, Matrix<p> mu, float rho){
@@ -239,7 +248,7 @@ class QP{
             return Nabla_xx_L+Nabla_xx_g;
         };
 
-        Matrix<nx, 1> newton_solve(Matrix<nx,1> x, Matrix<m,1> lambda, Matrix<p,1> mu, float rho){
+        Matrix<nx, 1> newton_solve(Matrix<nx,1> x, Matrix<m,1> lambda, Matrix<p,1> mu, float rho, bool verb){
             Matrix<nx,1> x_sol = x;
             for (int i = 0; i < max_iter_newton; i++){
 
@@ -252,9 +261,16 @@ class QP{
                     return x_sol;
                 }
                 Matrix<nx,nx> H = alhessian(x_sol, lambda, mu, rho);
+                
+                // If Hessian of augmented Lagrangian is rank defficient abort procedure 
+                if (Determinant(H)==0.0){
+                    x_sol.Fill(0.0/0.0);
+                    return x_sol;                    
+                }
                 Matrix<nx> Deltax = -Inverse(H)*g;
                 x_sol = x_sol+Deltax;    
             }
+            Serial.println("no convergence");
             return x_sol;
         };
     };
