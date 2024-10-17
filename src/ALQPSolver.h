a82072954a7b9bff293d5f76dcfbdf15f9cb9db2
adrianodelr <noche96@gmx.de>
2024-10-17 16:58:41 +0200

    added move/ copy constructors and assignment operators

--- src/ALQPSolver.h
+++ src/ALQPSolver.h
@@ -24,16 +24,15 @@ int freeMemory() {
 class QPparams {
 public:
     QPparams()
-        : max_iter_newton(10),
-          max_iter_outer(25),
+        : max_iter_outer(5),
+          max_iter_newton(10),
           max_iter_backtrack(10),          
           precision_newton(1e-5),
           precision_primal(1e-4),
           penalty_initial(1.0),
           penalty_scaling(10.0), 
-          backtrack_beta(0.8)           
+          backtrack_beta(0.8)
           {}  // Default values
-
     size_t max_iter_newton;
     size_t max_iter_outer;
     size_t max_iter_backtrack;
@@ -100,6 +99,12 @@ class QPsol{
         }          
 };
 
+// Set all matrix elements to zero 
+template<int Rows, int Cols, typename DType>
+void AllZero(Matrix<Rows, Cols, DType>& mat) {
+    mat.Fill(static_cast<DType>(0)); 
+}
+
 template<int nx, int m, int p, typename DType=float>
 class QP{
     public:
@@ -112,31 +117,113 @@ class QP{
             _A = A;
             _b = b;                        
             _G = G;
-            _h = h;            
+            _h = h;       
         }; 
+
         // Default constructor for empty qp
         QP(const String& mode = "") 
             : _params(new QPparams()), _alloc(true) {
-            init_QPmat();
+            QPZero();
             if (mode=="D") _debugging=true;
-            else _debugging=false;                                         
-        }; 
+            else _debugging=false;       
+        };
 
-        QP(const QPparams& parameters, const String& mode = "") 
-            : _params(&parameters), _alloc(false){
-            init_QPmat();
-            if (mode=="D") _debugging=true;
-            else _debugging=false;                                             
-        }
-
-        // destructor in case of taking default parameters 
+        // Destructor
         ~QP(){
             if(_alloc) delete _params;
         }
 
+        // Constructor accepting a QPparams object
+        QP(const QPparams& parameters, const String& mode = "") 
+            : _params(&parameters), _alloc(false){
+            QPZero();
+            if (mode=="D") _debugging=true;
+            else _debugging=false;                                             
+        }
+
+        // Move constructor 
+        QP(QP&& other) noexcept
+            : _params(other._params),
+              _alloc(other._alloc),
+              _debugging(other._debugging),
+              _Q(other._Q),
+              _q(other._q),
+              _A(other._A),
+              _b(other._b),
+              _G(other._G),
+              _h(other._h){
+            
+            other._params = nullptr;
+            other._alloc = false;
+            // reset other matrices to zero
+            QPZero(other);
+        };
+
+        // Move assignment operator
+        QP& operator=(QP&& other) noexcept {
+            if (this != &other) {
+
+                if (_alloc && _params != nullptr) {
+                    delete _params; 
+                }
+
+                _params = other._params;
+                _alloc = other._alloc;
+                _debugging = other._debugging;
+
+                _Q = other._Q;
+                _q = other._q;
+                _A = other._A;
+                _b = other._b;
+                _G = other._G;
+                _h = other._h;
+
+                other._params = nullptr;
+                other._alloc = false;
+                // reset other matrices to zero
+                QPZero(other);
+            }
+            return *this; 
+        }
+
+        // Copy constructor
+        QP(const QP& other)
+            : _alloc(other._alloc),  
+            _debugging(other._debugging),  
+            _Q(other._Q),  
+            _q(other._q),
+            _A(other._A),
+            _b(other._b),
+            _G(other._G),
+            _h(other._h){
+            _params = new QPparams(*other._params);  
+        }
+
+        // Copy assigment operator
+        QP& operator=(const QP& other) {
+            if (this != &other) { 
+
+                if (_params != nullptr) {
+                    delete _params; 
+                }
+
+                _alloc = other._alloc;
+                _debugging = other._debugging;
+                _Q = other._Q;  
+                _q = other._q;
+                _A = other._A;
+                _b = other._b;
+                _G = other._G;
+                _h = other._h;
+
+                _params = new QPparams(*other._params); 
+            }
+            return *this;  
+        }
+
         // solving the qp
         QPsol<nx,m,p,DType> solve(const String& mode = ""){
-            
+
             if (_debugging){
                 Serial.print("Free RAM when solving the QP: ");
                 Serial.print(freeMemory());            
@@ -150,9 +237,9 @@ class QP{
             Matrix<m ,1,DType> lambda;
             Matrix<p ,1,DType> mu;     
 
-            x.Fill(static_cast<DType>(0.0));
-            lambda.Fill(static_cast<DType>(0.0));
-            mu.Fill(static_cast<DType>(0.0));
+            AllZero(x);
+            AllZero(lambda);
+            AllZero(mu);
 
             DType rho = _params -> penalty_initial;
             DType Phi = _params -> penalty_scaling;
@@ -165,7 +252,7 @@ class QP{
             SolverStatus status;
 
             for (int i = 0; i < _params -> max_iter_outer; i++){
-                x = newton_solve(x, lambda, mu, rho, verb);
+                x = newton_solve(x, lambda, mu, rho);
                 if (isnan(x(0))){
                     // condition for rank deficient AL Hessian
                     status.degh = true;
@@ -176,7 +263,7 @@ class QP{
                     // reconsider if stationarity condition is satisfied to determine if x is truly the unconstrained optimum                                            
                     // otherwise re enter newton solver in next iteration
                     Nabla_x_L = stationarity(x, lambda, mu); 
-                    double gnorm = residual_norm(Nabla_x_L);                    
+                    double gnorm = residual_norm(Nabla_x_L); 
 
                     if (gnorm < _params -> precision_newton){
                         obj_val = objective(x);
@@ -235,32 +322,36 @@ class QP{
         bool _alloc; 
         bool _debugging; 
         
-        // initializes all matrices of given dimensions with zeros 
-        void init_QPmat(){
-            Matrix<nx,nx, DType> Q;
-            Matrix<nx, 1, DType> q;
-            Matrix<m, nx, DType> A;
-            Matrix<m,  1, DType> b;                        
-            Matrix<p, nx, DType> G;
-            Matrix<p,  1, DType> h; 
-
-            Q.Fill(static_cast<DType>(0.0));
-            q.Fill(static_cast<DType>(0.0));
-            A.Fill(static_cast<DType>(0.0));
-            b.Fill(static_cast<DType>(0.0));            
-            G.Fill(static_cast<DType>(0.0));           
-            h.Fill(static_cast<DType>(0.0));
-            update(Q, q, A, b, G, h);
+        // initializes all matrices of 'this' with zeros 
+        void QPZero(){
+            AllZero(_Q);
+            AllZero(_q);
+            AllZero(_A);
+            AllZero(_b);
+            AllZero(_G);
+            AllZero(_h);
         }
 
-        // returns 'left hand side' of the equality constraints   
+        // initializes all matrices of qp with zeros 
+        void QPZero(QP& qp){
+            AllZero(qp._Q);
+            AllZero(qp._q);
+            AllZero(qp._A);
+            AllZero(qp._b);
+            AllZero(qp._G);
+            AllZero(qp._h);
+        }
+
+        // returns residual vector of the equality constraints   
         inline Matrix<m,1, DType> c_eq(const Matrix<nx,1,DType>& x){
             return _A*x - _b; 
         };
-        // returns 'left hand side' of the inequality constraints   
+        
+        // returns residual vector of the inequality constraints   
         inline Matrix<p,1, DType> c_in(const Matrix<nx,1,DType>& x){
             return _G*x - _h; 
         };
+
         // returns norm of a residual vector   
         inline DType residual_norm(const Matrix<nx,1,DType>& g){
             return sqrt((~g * g)(0));
@@ -270,6 +361,7 @@ class QP{
         DType objective(const Matrix<nx,1,DType>& x){
             return 0.5*(~x*_Q*x)(0) + (~_q*x)(0);             
         };
+
         // gradient of Lagrangian 
         Matrix<nx,1,DType> stationarity(const Matrix<nx,1,DType>& x, const Matrix<m,1,DType>& lambda, const Matrix<p,1,DType>& mu){
             Matrix<nx,1,DType> Nabla_x_L = _Q*x + _q; 
@@ -330,14 +422,12 @@ class QP{
         // gradient of augmented Lagrangian 
         void algradient(Matrix<nx,1,DType>& g, const Matrix<nx,1,DType>& x, const Matrix<m,1,DType>& lambda, const Matrix<p,1,DType>& mu, const DType& rho){
             g = stationarity(x, lambda, mu);
-            if (m+p != 0){
-                if(m != 0){
-                    g += (~_A*rho) * c_eq(x);    
-                }
-                if(p != 0){
-                    Matrix<p,p,DType> Ip = active_ineq(x, mu, rho);
-                    g += (~_G*Ip) * c_in(x);
-                }
+            if(m != 0){
+                g += (~_A*rho) * c_eq(x);    
+            }
+            if(p != 0){
+                Matrix<p,p,DType> Ip = active_ineq(x, mu, rho);
+                g += (~_G*Ip) * c_in(x);
             }
             // Nabla_x_g = (~_A*rho || ~_G*Ip) * primal_residual(x, lambda, mu); 
         };
@@ -355,7 +445,7 @@ class QP{
         };        
 
         // 'Inner' newton solver 
-        Matrix<nx, 1, DType> newton_solve(const Matrix<nx,1,DType>& x, const Matrix<m,1,DType>& lambda, const Matrix<p,1,DType>& mu, const DType& rho, const bool& verb){
+        Matrix<nx, 1, DType> newton_solve(const Matrix<nx,1,DType>& x, const Matrix<m,1,DType>& lambda, const Matrix<p,1,DType>& mu, const DType& rho){
             
             Matrix<nx,1,DType> x_sol = x;            
             Matrix<nx,1,DType> Deltax; 
@@ -365,7 +455,7 @@ class QP{
             Matrix<nx,nx,DType> H; 
 
             for (int i = 0; i < _params -> max_iter_newton; i++){
-                
+
                 algradient(g, x_sol, lambda, mu, rho);
                 DType gnorm = residual_norm(g);
 
